be81c2357984633d583b80a268b8f17d0d947681
3chordme <3chordme@gmail.com>
2016-01-04 14:27:40 -0700

    I just learned something awesome

--- 04.1.mini-Routing/js/app.js
+++ 04.1.mini-Routing/js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module("miniRouting", []);
+var app = angular.module('miniRouting', ['ui-router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'homeCtrl'
+    })
+    .state('products', {
+      url: '/products/:id',
+      templateUrl: 'js/products/products.html',
+      controller: 'productsCtrl'
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'js/settings/settings.html',
+      controller: 'settingsCtrl'
+    });
+
+  $urlRouterProvider
+  .otherwise('/');
+
+
+});
